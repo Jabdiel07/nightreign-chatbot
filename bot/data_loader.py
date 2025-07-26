62714cbabba71c47265ac3550b8339d300ee82ff
Jabdiel_PR 🔥⚔ <Jabdiel2015@gmail.com>
2025-07-26 16:02:22 -0400

    Updated .gitignore and created game_info.txt and main.py

---
+++ bot/data_loader.py
@@ -0,0 +1,6 @@
+import csv
+from pathlib import Path
+from typing import Dict, List, Tuple
+
+def load_game_data(path: str="data\game_info.csv") -> Tuple[Dict[str,dict], Dict[str,List[str]]]:
+    pass
