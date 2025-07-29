19189733798d4899c199a8be81580cb4bfc64981
Jabdiel_PR 🔥⚔ <Jabdiel2015@gmail.com>
2025-07-29 12:35:46 -0400

    removed excessive comments in main.py and data_loader.py

--- bot/data_loader.py
+++ bot/data_loader.py
@@ -3,36 +3,19 @@ from pathlib import Path
 from typing import Dict, List, Tuple
 
 def load_game_data(path: str="data\game_info.csv") -> Tuple[Dict[str,dict], Dict[str,List[str]]]:
-    name_lookup: Dict[str, dict] = {} # this will map each character/boss's name and its full row. This is the same as running name_lookup = {}, the way we originally have it set up as is using type hints
-    category_lookup: Dict[str, List[str]] = {} # this will map each category field to a list of all names in that category. Same thing as category_lookup = {}
+    name_lookup: Dict[str, dict] = {} # maps each character/boss name to their respective row
+    category_lookup: Dict[str, List[str]] = {} # maps each character/boss names to their respective category into a list
 
-    csv_path = Path(path) # locates the path of the csv file
-    with csv_path.open(newline="", encoding="utf-8") as f: # this allows us to open the file and read it. When working with csv files, it's good practice to always add newline = '' as an argument
+    # locates the path of the csv file
+    csv_path = Path(path)
+    with csv_path.open(newline="", encoding="utf-8") as f:
         reader = csv.DictReader(f) # DictReader reads each line as a dict where the key is the top row in the file and the values are the rest of the rows
 
         for row in reader:
-            name = row["name"].strip() # strip will remove the leading and trailing whitespaces in the string
-            category = row["category"].strip().lower() # we want to make sure the dictionary keys are always consistent, so we enforce it by using .lower()
-            name_lookup[name.lower()] = row # don't need to do +=, in dictionaries, a simple = will keep adding to the dictionary and won't overwrite anything
-            '''
-            name_lookup[name.lower()]
-
-            - name_lookup["wylder"]
-
-            name_lookup["wylder"] = row
-
-            - {
-                "wylder": {
-                    "name": "Wylder",
-                    "category": "character",
-                    "lore": "...",
-                    etc
-                }
-            }
-
-            This way, if the user does something like /info Wylder lore, the bot can access the Wylder key and from it's values, select the appropriate key with that keys value
-            '''
-            category_lookup.setdefault(category, []).append(name) # setdefault checks if the category already exists in category_lookup, if it doesn't, then it adds it as a key. If it exists, then it keeps going with the rest of the operation. The rest of the operation is adding the name of the character we're currently on into a list. This will create a dictionary with two keys, character and boss. Each of these keys will hold a list as their values, the character key will have a list holding the name of all of the characters and the boss key will hold a list with all the names of the bosses
+            name = row["name"].strip()
+            category = row["category"].strip().lower()
+            name_lookup[name.lower()] = row
+            category_lookup.setdefault(category, []).append(name) # setdefault checks if the category already exists in category_lookup, if it doesn't, then it adds it as a key. If it exists, then it keeps going with the rest of the operation
     
     return name_lookup, category_lookup
 
--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,4 @@
 from discord.commands import Option, SlashCommandGroup
-#from logging_config import setup_logging
 import discord
 from discord.ext import commands
 import os
@@ -19,15 +18,18 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix ="!", intents=intents)
 
-name_lookup, category_lookup = load_game_data() # loading the results of the load_game_data function into two dictionaries respectively
+# load the results of the load_game_data function into two dictionaries respectively
+name_lookup, category_lookup = load_game_data()
 
-char_fields = ["description", "lore", "weapon_preference", "passive_ability", "skill_ability", "ultimate_art", "scaling"] # list that contains the fields we care about for characters
+# create two lists that contain character and boss fields respectively
+char_fields = ["description", "lore", "weapon_preference", "passive_ability", "skill_ability", "ultimate_art", "scaling"]
 
-boss_fields = ["description", "lore", "weakness", "immunity", "resistance"] # list that contains the fields we care about for bosses
+boss_fields = ["description", "lore", "weakness", "immunity", "resistance"]
 
-category = SlashCommandGroup("category", "Get info on a character or boss.", guild_ids = [612816995754180655]) # this is creating the base format for the slash command. The first argument is the name we're going to use to invoke the slash command, so /category in this case, and the second argument is the description that appears to the user that tell them what the command does. The third argument sets the server id so it doesn't take too long for the slash commands to update
+# base format for the slash command, it also sets the server id where the bot will be used at and more can be added to it
+category = SlashCommandGroup("category", "Get info on a character or boss.", guild_ids = [612816995754180655])
 
-# this event right here is tied to the character choice. The function does the usual, it asks the user for which character they want info for and then they pick the field they're interested in.
+# event for character choice
 @category.command(name="character", description = "Look up a Nightfarer's attributes.")
 async def character_info(ctx, name = Option(str, "Which character?", choices = category_lookup["character"]), field = Option(str, "Which field to show?", choices = char_fields)):
     logger.info(f"{field} Query initiated by {ctx.author.name} for {name}")
@@ -36,7 +38,7 @@ async def character_info(ctx, name = Option(str, "Which character?", choices = c
     pretty_field = field.replace("_", " ").title() #.title() converts the field string into a title case, capitalizing the first letter of each word and lowercases the rest
     await ctx.respond(f"**{name}** — *{pretty_field}*: {value}")
 
-# same as the previous event, but this time for bosses
+# event for boss choice
 @category.command(name="boss", description = "Look up a Nightlord's attribute.")
 async def boss_info(ctx, name = Option(str, "Which boss", choices = category_lookup["boss"]), field = Option(str, "Which field to show?", choices = boss_fields)):
     logger.info(f"{field} Query initiated by {ctx.author.name} for {name}")
@@ -45,11 +47,12 @@ async def boss_info(ctx, name = Option(str, "Which boss", choices = category_loo
     pretty_field = field.replace("_", " ").title()
     await ctx.respond(f"**{name}** — *{pretty_field}*: {value}")
 
-bot.add_application_command(category) # this line right here ensures that both slash commands we defined for characters and bosses are registered to the Discord bot. If we declared the slash command like we did in the translator bot (@bot.slash_command(...)), then Discord would be able to detect the slash command automatically. Since we defined the slash command in a different way this time, then this line is necessary so Discord can recognize the group (that's the category = SlashCommandGroup we defined) and its subcommands (the character and boss functions)
+# ensure we register the character and boss commands to the Discord bot
+bot.add_application_command(category)
 
 @bot.event
 async def on_ready():
-    await bot.sync_commands() # this line is optional, pretty much all it does is register all the commands you defined a lot faster (without this line, it could take up to an hour for them to register)
+    await bot.sync_commands() # registers the defined commands in a timely manner
     logger.info(f"{bot.user} has connected to Discord.")
 
 @bot.command(name = "hello")
